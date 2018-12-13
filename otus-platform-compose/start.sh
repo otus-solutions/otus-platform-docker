397b5218cdd18c2193861057b7bcf2e5f635d1c1
Tiago Matana <tiago.matana@gmail.com>
2018-12-13 16:44:25 -0200

    Metodologia CopyOnWrite implementada

--- otus-platform-compose/start.sh
+++ otus-platform-compose/start.sh
@@ -93,10 +93,11 @@ read ANSWER
 printf "\n${DARKGRAY}#############${NC} MONGODB${DARKGRAY} #############${NC}\n"
 printf "${GREEN}"
 printf "Atualizando passwords mongodb..."
-cp ./mongodb/example.js ./mongodb/otus-db-build.js
-sed -i "s/PASS_ADMIN/$passAdmin/g" ./mongodb/otus-db-build.js
-sed -i "s/PASS_OTUS/$passOtus/g" ./mongodb/otus-db-build.js
-sed -i "s/PASS_DOMAIN/$passDomain/g" ./mongodb/otus-db-build.js
+cp ./mongodb/config/example-initdb.js ./mongodb/persistence/otus-db-build.js
+cp ./mongodb/config/mongod.conf ./mongodb/persistence/mongod.conf
+sed -i "s/PASS_ADMIN/$passAdmin/g" ./mongodb/persistence/otus-db-build.js
+sed -i "s/PASS_OTUS/$passOtus/g" ./mongodb/persistence/otus-db-build.js
+sed -i "s/PASS_DOMAIN/$passDomain/g" ./mongodb/persistence/otus-db-build.js
 sleep 1
 printf "\n[COMPLETO]\n\n"
 printf "${NC}"
@@ -108,36 +109,8 @@ sed -i -E "s/WILDFLY_USER=.+/WILDFLY_USER=$user_wildfly/g" .env
 sed -i -E "s/WILDFLY_PASS=.+/WILDFLY_PASS=$pass_wildfly/g" .env
 
 sudo rm -rf ./wildfly/persistence/wildfly
-
-
-sleep 1
-printf "\n[COMPLETO]\n\n"
-printf "${NC}"
-
-printf "${DARKGRAY}#############${NC} NGINX${DARKGRAY} #############${NC}\n"
-printf "${GREEN}"
-printf "Gerando arquivos de host...\n"
-cp ./nginx/sites-available/otus.template ./nginx/sites-enabled/otus.conf
-cp ./nginx/sites-available/api-otus.template ./nginx/sites-enabled/api-otus.conf
-cp ./nginx/sites-available/domain.template ./nginx/sites-enabled/domain.conf
-cp ./nginx/sites-available/api-domain.template ./nginx/sites-enabled/api-domain.conf
-cp ./nginx/sites-available/studio.template ./nginx/sites-enabled/studio.conf
-cp ./nginx/sites-available/assets.template ./nginx/sites-enabled/assets.conf
-# cp ./nginx/sites-available/localhost.template ./nginx/conf.d/default.conf
-sleep 1
-printf "Atualizando server names...\n"
-sed -i "s/localhost/$otus/g" ./nginx/sites-enabled/otus.conf
-sed -i "s/localhost/$domain/g" ./nginx/sites-enabled/domain.conf
-sed -i "s/localhost/$studio/g" ./nginx/sites-enabled/studio.conf
-sed -i "s/localhost/$assets/g" ./nginx/sites-enabled/assets.conf
-sed -i "s/localhost/$otus_api/g" ./nginx/sites-enabled/api-otus.conf
-sed -i "s/localhost/$domain_api/g" ./nginx/sites-enabled/api-domain.conf
-sleep 1
-printf "\n[COMPLETO]\n\n"
-printf "${NC}"
-
-printf "***${BLUE}FIM DA CONFIGURAÇÃO${NC}***\n\n"
-
+mkdir -p ./wildfly/persistence/wildfly/conf
+cp ./wildfly/config/standalone.xml ./wildfly/persistence/wildfly/conf/standalone.xml
 mkdir -p ./wildfly/persistence/wildfly/bin
 cp ./wildfly/config/standalone.conf ./wildfly/persistence/wildfly/bin/standalone.conf
 case $debug in
@@ -149,10 +122,39 @@ case $debug in
         ;;
 esac
 
-mkdir -p ./wildfly/persistence/wildfly/conf
-cp ./wildfly/config/standalone.xml ./wildfly/persistence/wildfly/conf/standalone.xml
+sleep 1
+printf "\n[COMPLETO]\n\n"
+printf "${NC}"
+
+printf "${DARKGRAY}#############${NC} NGINX${DARKGRAY} #############${NC}\n"
+printf "${GREEN}"
+printf "Gerando arquivos de host...\n"
+sudo rm -rf ./nginx/persistence/*
+mkdir -p ./nginx/persistence/sites-enabled/
+cp ./nginx/config/sites-available/otus.template ./nginx/persistence/sites-enabled/otus.conf
+cp ./nginx/config/sites-available/api-otus.template ./nginx/persistence/sites-enabled/api-otus.conf
+cp ./nginx/config/sites-available/domain.template ./nginx/persistence/sites-enabled/domain.conf
+cp ./nginx/config/sites-available/api-domain.template ./nginx/persistence/sites-enabled/api-domain.conf
+cp ./nginx/config/sites-available/studio.template ./nginx/persistence/sites-enabled/studio.conf
+cp ./nginx/config/sites-available/assets.template ./nginx/persistence/sites-enabled/assets.conf
+cp -R ./nginx/config/ssl/ ./nginx/persistence/
+cp ./nginx/config/nginx.conf ./nginx/persistence/nginx.conf
+sleep 1
+printf "Atualizando server names...\n"
+sed -i "s/localhost/$otus/g" ./nginx/persistence/sites-enabled/otus.conf
+sed -i "s/localhost/$domain/g" ./nginx/persistence/sites-enabled/domain.conf
+sed -i "s/localhost/$studio/g" ./nginx/persistence/sites-enabled/studio.conf
+sed -i "s/localhost/$assets/g" ./nginx/persistence/sites-enabled/assets.conf
+sed -i "s/localhost/$otus_api/g" ./nginx/persistence/sites-enabled/api-otus.conf
+sed -i "s/localhost/$domain_api/g" ./nginx/persistence/sites-enabled/api-domain.conf
+sleep 1
+printf "\n[COMPLETO]\n\n"
+printf "${NC}"
+
+printf "***${BLUE}FIM DA CONFIGURAÇÃO${NC}***\n\n"
+
 sudo docker-compose up -d
 sudo chmod -R 775 ./wildfly/persistence
-sudo chmod -R 775 ./nginx/html
-sudo chmod -R 775 ./nginx/sites-enabled
+sudo chmod -R 775 ./nginx/persistence/html
+sudo chmod -R 775 ./nginx/persistence/sites-enabled
 sudo docker ps

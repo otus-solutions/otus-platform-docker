f8d5b20f94359cd5cf14ddced3782147a4f73ba3
Adriano <desenvacc@gmail.com>
2019-04-03 17:37:05 -0300

    Creação de todas as collections no script initDb

--- otus-platform-compose/mongodb/config/example-initdb.js
+++ otus-platform-compose/mongodb/config/example-initdb.js
@@ -33,23 +33,54 @@ db.createUser({
 });
 
 db = db.getSiblingDB('otus');
-db.getCollection("activity").createIndex({ "surveyForm.surveyTemplate.identity.acronym": 1 })
-db.getCollection("activity").createIndex({ "participantData.recruitmentNumber": 1 })
-db.getCollection("activity").createIndex({ "participantData.fieldCenter.acronym": 1 })
-db.getCollection("activity").createIndex({ "isDiscarded": 1 })
-db.getCollection("activity").createIndex({ "category": 1 })
-db.getCollection("activity").createIndex({ "isDiscarded": 1, "participantData.fieldCenter.acronym": 1 })
 
-db.getCollection("aliquot").createIndex({ "code": 1 })
-db.getCollection("aliquot").createIndex({ "transportationLotId": 1 })
-db.getCollection("aliquot").createIndex({ "examLotId": 1 })
+db.createCollection("activity");
+db.createCollection("activity_configuration");
+db.createCollection("activity_inapplicability");
+db.createCollection("activity_permission");
+db.createCollection("activity_revision");
+db.createCollection("aliquot");
+db.createCollection("auditor");
+db.createCollection("datasource");
+db.createCollection("datasource_temp");
+db.createCollection("exam_lot");
+db.createCollection("exam_result");
+db.createCollection("exam_sending_lot");
+db.createCollection("field_center");
+db.createCollection("fs.files");
+db.createCollection("laboratory_configuration");
+db.createCollection("participant");
+db.createCollection("participant_laboratory");
+db.createCollection("participant_quality_control");
+db.createCollection("password_reset_control");
+db.createCollection("project_configuration");
+db.createCollection("report");
+db.createCollection("survey");
+db.createCollection("survey_group");
+db.createCollection("system_config");
+db.createCollection("transportation_lot");
+db.createCollection("unnecessary");
+db.createCollection("user");
+db.createCollection("user_permission");
+db.createCollection("user_permission_profile");
+
+db.getCollection("activity").createIndex({ "surveyForm.surveyTemplate.identity.acronym": 1 });
+db.getCollection("activity").createIndex({ "participantData.recruitmentNumber": 1 });
+db.getCollection("activity").createIndex({ "participantData.fieldCenter.acronym": 1 });
+db.getCollection("activity").createIndex({ "isDiscarded": 1 });
+db.getCollection("activity").createIndex({ "category": 1 });
+db.getCollection("activity").createIndex({ "isDiscarded": 1, "participantData.fieldCenter.acronym": 1 });
+
+db.getCollection("aliquot").createIndex({ "code": 1 });
+db.getCollection("aliquot").createIndex({ "transportationLotId": 1 });
+db.getCollection("aliquot").createIndex({ "examLotId": 1 });
 
 db.getCollection("exam_result").createIndex({ "recruitmentNumber": 1 });
-db.getCollection("exam_result").createIndex({ "examSendingLotId": 1 })
-db.getCollection("exam_result").createIndex({ "objectType": 1 })
-db.getCollection("exam_result").createIndex({ "examName": 1 })
-db.getCollection("exam_result").createIndex({ "recruitmentNumber": 1 })
-db.getCollection("exam_result").createIndex({ "aliquotCode": 1 })
+db.getCollection("exam_result").createIndex({ "examSendingLotId": 1 });
+db.getCollection("exam_result").createIndex({ "objectType": 1 });
+db.getCollection("exam_result").createIndex({ "examName": 1 });
+db.getCollection("exam_result").createIndex({ "recruitmentNumber": 1 });
+db.getCollection("exam_result").createIndex({ "aliquotCode": 1 });
 
 db.getCollection("participant").createIndex({ "recruitmentNumber": 1 });
 
@@ -58,6 +89,16 @@ db.getCollection("participant_laboratory").createIndex({ "recruitmentNumber": 1 
 db.getCollection("filestore.files").createIndex({ "filename": 1, "uploadDate": 1 });
 
 db.getCollection("fs.files").createIndex({ "filename": 1 });
+
+db.getCollection("user_permission_profile").insert({
+    "name" : "DEFAULT",
+    "permissions" : [
+        {
+            "objectType" : "SurveyGroupPermission",
+            "groups" : []
+        }
+    ]
+});
 
 db.system.js.save({_id: "syncResults", value: function () {
         var AliquotExamCorrelation = db.laboratory_configuration.findOne({objectType:"AliquotExamCorrelation"});
